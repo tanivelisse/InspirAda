47af071b9c2d6abbfa00fbf83b5a28fee25f4f65
Tania Medina <tanivelisse@gmail.com>
2019-06-05 10:04:09 -0500

    register component handleChange done

--- src/Register/index.js
+++ src/Register/index.js
@@ -10,26 +10,28 @@ class Register extends Component {
 		      about:''
 		}
 	}
-	handleChange() {
+	handleChange = (e) => {
 		console.log("handleChange was called");
+		this.setState({
+			[e.target.name]: e.target.value
+		})
 	}
 	render() {
+		console.log(this.state);
     return (
       <div className="Register">
         <h1>Register</h1>
 	        <form>
-	        email:
-	        <input type="email" name='email' onChange={this.handleChange}/><br/>
-	        username: 
-	        <input type="text" name='username' onChange={this.handleChange}/><br/>
-	        password:
-	        <input type="password" name='password'  onChange={this.handleChange}/><br/>
-	        about:
-	        <input type="text" name='username' onChange={this.handleChange}/><br/>
-	        <button>Register</button>
+		        email:
+		        <input type="email" name='email' onChange={this.handleChange}/><br/>
+		        username: 
+		        <input type="text" name='username' onChange={this.handleChange}/><br/>
+		        password:
+		        <input type="password" name='password'  onChange={this.handleChange}/><br/>
+		        about:
+		        <input type="text" name='about' onChange={this.handleChange}/><br/>
+		        <button>Register</button>
 	        </form>
-
-        
       </div>
     );
   }
