3e24282ad34a839311964286546f292ff5f94598
Tania Medina <tanivelisse@gmail.com>
2019-06-06 10:54:31 -0500

    added allpost component,getAllPosts fetch call

---
+++ src/AllPosts/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class AllPosts extends Component {
+	
+	render(){
+		return(
+			<h1>All Posts</h1>
+		)
+	}
+}
+
+export default AllPosts;
--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -2,14 +2,46 @@ import React, {Component} from 'react';
 import Register from '../Register';
 import Login from '../Login';
 import Logout from '../Logout';
+import AllPosts from '../AllPosts';
 
 class HomePage extends Component {
+	constructor(){
+		super()
+		this.state = {
+			posts:[]
+		}
+	}
+	componentDidMount(){
+		this.getAllPosts()
+	}
+	//GET ALL POSTS
+	getAllPosts = async()=>{
+		console.log('getAllPosts was called');
+		let parseResponse = null;
+		try{
+			const postsResponse = await fetch(process.env.REACT_APP_SERVER_URL + "/api/v1/users",{
+				credentials: "include",
+				headers: {
+				"Content-Type":"application/Jason"
+				}
+			});
+			parseResponse = await postsResponse.json();
+        	console.log(parseResponse);
+
+		}catch(err){
+			console.log(err);
+		}
+		this.setState({
+			posts: parseResponse
+		})
+	}
 	render(){
 		return(
 			<div className="App">
 		        <Register masterLogin={this.props.masterLogin}/>
 		        <Login masterLogin={this.props.masterLogin}/>
 		        <Logout masterLogout={this.props.masterLogout}/>
+		        <AllPosts/>
 		    </div>
 		)
 	}
